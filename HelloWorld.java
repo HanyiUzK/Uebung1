72bb9154995c7d116d92fe5d0f32830d7056d05a
hzhu2 <hzhu2@smail.uni-koeln.de>
2020-11-05 22:35:32 +0800

    Jahre ausgegeben

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 
   public class void main(String[] args){
-    System.out.println("Hello World!");
+    System.out.println("Ich bin schon 4 Jahre an der Uni K?ln.");
   }
 }
